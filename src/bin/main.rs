4d512026655e87187e31f85205153f27b4d43132
Theora59-dev <clem6kiruagon@gmx.fr>
2026-06-01 00:40:16 +0200

    correction coté éditeur

---
+++ src/bin/main.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::gpio::{Level, Output, OutputConfig};
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant};
+use esp_println::println;
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[main]
+fn main() -> ! {
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+
+    let _peripherals = esp_hal::init(config);
+    let config = OutputConfig::default();
+    let mut led = Output::new(_peripherals.GPIO2, Level::Low, config);
+
+    println!("ESP32 démarré !");
+
+    let mut compteur = 0u32;
+
+    loop {
+        compteur += 1;
+        println!("Compteur = {}", compteur);
+        led.toggle();
+
+        let start = Instant::now();
+        while start.elapsed() < Duration::from_secs(5) {}
+    }
+}
